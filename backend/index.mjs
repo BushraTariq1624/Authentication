101d15c4f4f681a260bfcc2b0075f772e41e2515
BushraTariq1624 <tbushra211@gmail.com>
2025-04-24 22:07:01 +0500

    changes

--- backend/index.mjs
+++ backend/index.mjs
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5173','https://authentication-frontend-psi.vercel.app/'],
+		origin: ['http://localhost:5173','https://authentication-frontend-psi.vercel.app'],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
 		credentials: true,
 	}),

564501f74f7f27f0832dd2c2117c6684f52ee57e
BushraTariq1624 <tbushra211@gmail.com>
2025-04-24 14:24:44 +0500

    Commit

--- backend/index.mjs
+++ backend/index.mjs
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5173','https://authentication-chi-dusky.vercel.app','https://authentication-hmh6.vercel.app'],
+		origin: ['https://authentication-hmh6.vercel.app'],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
 		credentials: true,
 	}),
